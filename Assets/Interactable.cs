d9e6a224079563182ff7e874b9e87defc4c767d0
jaakop <jaakko@kronogra.fi>
2026-01-30 20:45:47 +0200

    show interaction indicator

---
+++ Assets/Interactable.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class Interactable : MonoBehaviour
+{
+    public UnityEvent OnInteract = new();
+
+    [SerializeField] private GameObject _interactIndicator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _interactIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag != "Player")
+        {
+            return;
+        }
+        
+        _interactIndicator.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.tag != "Player")
+        {
+            return;
+        }
+        
+        _interactIndicator.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        OnInteract.Invoke();
+    }
+}
