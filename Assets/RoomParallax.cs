d287100a9297a74a2fea1638db1bdf02ca255040
jaakop <jaakko@kronogra.fi>
2026-01-31 19:29:45 +0200

    parrallax makes me lose my mind

--- Assets/RoomParallax.cs
+++ Assets/RoomParallax.cs
@@ -12,6 +12,7 @@ public class RoomParallax : MonoBehaviour
     private float rightInteriorPos;
 
     [SerializeField] float parralaxStrength;
+    [SerializeField] float interiorWallParralxStrength;
 
     private Camera camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,14 +33,14 @@ public class RoomParallax : MonoBehaviour
         var rightSqew = Mathf.Lerp(0, rightScale, rightParrallax);
         rightWall.localScale = new Vector3(rightSqew, rightWall.localScale.y, 1);
         
-        rightWallInterior.position =  new Vector3(rightInteriorPos + Mathf.Lerp(0, rightScale, rightParrallax), rightWallInterior.position.y, rightWallInterior.position.z);
+        rightWallInterior.position =  new Vector3(rightInteriorPos + Mathf.Lerp(0, rightScale, rightParrallax) * interiorWallParralxStrength, rightWallInterior.position.y, rightWallInterior.position.z);
         rightWallInterior.gameObject.SetActive(rightWall.position.x - camera.transform.position.x > -1);
         
         var leftParrallax = Mathf.Clamp((camera.transform.position.x - leftWall.position.x) / parralaxStrength, 0, 1);
         var leftSqew = Mathf.Lerp(0, leftScale, leftParrallax);
         leftWall.localScale = new Vector3(leftSqew, leftWall.localScale.y, 1);
         
-        leftWallInterior.position =  new Vector3(leftInteriorPos + Mathf.Lerp(0, -leftScale, leftParrallax), leftWallInterior.position.y, leftWallInterior.position.z);
+        leftWallInterior.position =  new Vector3(leftInteriorPos + Mathf.Lerp(0, -leftScale, leftParrallax) * interiorWallParralxStrength, leftWallInterior.position.y, leftWallInterior.position.z);
         leftWallInterior.gameObject.SetActive(camera.transform.position.x - leftWall.position.x > -1);
     }
 }
