26103f37c8f56a37b330153d00c4d3cd3ef9246b
jaakop <jaakko@kronogra.fi>
2026-01-31 13:20:15 +0200

    polish parallax

--- Assets/RoomParallax.cs
+++ Assets/RoomParallax.cs
@@ -31,12 +31,15 @@ public class RoomParallax : MonoBehaviour
         var rightParrallax = Mathf.Clamp((rightWall.position.x - camera.transform.position.x) / parralaxStrength, 0, 1);
         var rightSqew = Mathf.Lerp(0, rightScale, rightParrallax);
         rightWall.localScale = new Vector3(rightSqew, rightWall.localScale.y, 1);
-        rightWallInterior.position =  new Vector3(rightInteriorPos + Mathf.Lerp(0, rightScale, rightParrallax), rightWallInterior.position.y, rightWallInterior.position.z);
-
         
-        var leftParrallax = Mathf.Clamp((camera.transform.position.x- leftWall.position.x) / parralaxStrength, 0, 1);
+        rightWallInterior.position =  new Vector3(rightInteriorPos + Mathf.Lerp(0, rightScale, rightParrallax), rightWallInterior.position.y, rightWallInterior.position.z);
+        rightWallInterior.gameObject.SetActive(rightWall.position.x - camera.transform.position.x > -1);
+        
+        var leftParrallax = Mathf.Clamp((camera.transform.position.x - leftWall.position.x) / parralaxStrength, 0, 1);
         var leftSqew = Mathf.Lerp(0, leftScale, leftParrallax);
         leftWall.localScale = new Vector3(leftSqew, leftWall.localScale.y, 1);
+        
         leftWallInterior.position =  new Vector3(leftInteriorPos + Mathf.Lerp(0, -leftScale, leftParrallax), leftWallInterior.position.y, leftWallInterior.position.z);
+        leftWallInterior.gameObject.SetActive(camera.transform.position.x - leftWall.position.x > -1);
     }
 }
