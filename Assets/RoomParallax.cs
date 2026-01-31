1472fb5e3c6a4891709b82fa229c2311657d80e7
jaakop <jaakko@kronogra.fi>
2026-01-31 09:12:20 +0200

    parrallax magik

--- Assets/RoomParallax.cs
+++ Assets/RoomParallax.cs
@@ -4,8 +4,12 @@ public class RoomParallax : MonoBehaviour
 {
     [SerializeField] Transform leftWall;
     private float leftScale;
+    [SerializeField] Transform leftWallInterior;
+    private float leftInteriorPos;
     [SerializeField] Transform rightWall;
     private float rightScale;
+    [SerializeField] Transform rightWallInterior;
+    private float rightInteriorPos;
 
     [SerializeField] float parralaxStrength;
 
@@ -16,16 +20,23 @@ public class RoomParallax : MonoBehaviour
         camera = Camera.main;
         leftScale = leftWall.localScale.x;
         rightScale = rightWall.localScale.x;
+
+        leftInteriorPos = leftWallInterior.position.x;
+        rightInteriorPos = rightWallInterior.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var rightParallax = Mathf.Lerp(0, rightScale, Mathf.Clamp((rightWall.position.x - camera.transform.position.x) / parralaxStrength, 0, 1));
-        rightWall.localScale = new Vector3(rightParallax, rightWall.localScale.y, 1);
+        var rightParrallax = Mathf.Clamp((rightWall.position.x - camera.transform.position.x) / parralaxStrength, 0, 1);
+        var rightSqew = Mathf.Lerp(0, rightScale, rightParrallax);
+        rightWall.localScale = new Vector3(rightSqew, rightWall.localScale.y, 1);
+        rightWallInterior.position =  new Vector3(rightInteriorPos + Mathf.Lerp(0, rightScale, rightParrallax), rightWallInterior.position.y, rightWallInterior.position.z);
 
         
-        var leftParallax = Mathf.Lerp(0, leftScale, Mathf.Clamp((camera.transform.position.x- leftWall.position.x) / parralaxStrength, 0, 1));
-        leftWall.localScale = new Vector3(leftParallax, leftWall.localScale.y, 1);
+        var leftParrallax = Mathf.Clamp((camera.transform.position.x- leftWall.position.x) / parralaxStrength, 0, 1);
+        var leftSqew = Mathf.Lerp(0, leftScale, leftParrallax);
+        leftWall.localScale = new Vector3(leftSqew, leftWall.localScale.y, 1);
+        leftWallInterior.position =  new Vector3(leftInteriorPos + Mathf.Lerp(0, -leftScale, leftParrallax), leftWallInterior.position.y, leftWallInterior.position.z);
     }
 }
